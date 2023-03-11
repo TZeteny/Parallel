cf0468f6626c731c267917e4bc8ce0466539b789
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-03-11 13:00:55 +0100

    Number generation and parameter reading in separate functions.

--- 1.het/1-5.c
+++ 1.het/1-5.c
@@ -2,37 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Needed to return multiple values in read().
+struct parameters {
+    int number_count, min, max;
+};
+typedef struct parameters Struct;
+
+Struct read();
+void random_number();
+
 int main() {
-  srand(time(NULL));
-  
-  int min, max;
-  int howMany;
-  
-  int ok;
-  char ch;
-  do{
+  Struct s = read();
+
+  random_number(s);
+
+  return 0;
+}
+
+Struct read() {
+    Struct s;
+    int ok;
+    char ch;
+
+    do{
       ok = 1;
       printf("How many numbers to generate: ");
-      if( scanf("%d", &howMany)!=1 ) {
+      if( scanf("%d", &s.number_count)!=1 ) {
           printf("Wrong input!\n");
           ok = 0;
           while ((ch=getchar())!='\n') ;
       }
       else {
           printf("Minimum, maximum: ");
-          if( scanf("%d, %d", &min, &max)!=2 ) {
+          if( scanf("%d, %d", &s.min, &s.max)!=2 ) {
           printf("Wrong input!\n");
           ok = 0;
           while ((ch=getchar())!='\n') ;
-      }
+          }
       }
   }while(!ok);
-  
-  int i;
-  while(i < howMany) {
-      printf("%d\n", (rand() % (max - min + 1)) + min);
+
+  return s;
+}
+
+void random_number(Struct s) {
+    srand(time(NULL));
+    int i = 0;
+
+    printf("The random numbers:\n");
+    while(i < s.number_count) {
+      printf("%d\n", (rand() % (s.max - s.min + 1)) + s.min);
       i++;
-  }
+    }
+}
 
   return 0;
 }
