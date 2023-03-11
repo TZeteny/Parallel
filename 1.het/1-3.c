9c227117577407242cf3f42ca61256f22ba6b610
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-03-11 12:04:33 +0100

    1-3 Sleep works on both Linux and Windows now.

--- 1.het/1-3.c
+++ 1.het/1-3.c
@@ -1,10 +1,27 @@
-#include <windows.h>
+#ifdef _WIN32
+//  For Windows (32- and 64-bit)
+#   include <windows.h>
+#   define SLEEP(msecs) Sleep(msecs)
+#elif __unix
+//  For linux, OSX, and other unixes
+#   define _POSIX_C_SOURCE 199309L // or greater
+#   include <time.h>
+#   define SLEEP(msecs) do {            \
+        struct timespec ts;             \
+        ts.tv_sec = msecs/1000;         \
+        ts.tv_nsec = msecs%1000*1000;   \
+        nanosleep(&ts, NULL);           \
+        } while (0)
+#else
+#   error "Unknown system"
+#endif
+
 #include <stdio.h>
 
 int main() {
-  printf("I'm sleepy...\n");
-  Sleep(1000);
-  printf("Not anymore.\n");
+    printf("I'm sleepy...\n");
+    SLEEP(2000);
+    printf("Not anymore.");
 
-  return 0;
+    return 0;
 }
