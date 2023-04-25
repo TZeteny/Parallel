4beab7a39e66943b479587d24af48053cd95534b
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-04-25 21:22:07 +0200

    Main thread does stuff for 8 seconds, child for 4. Then vice versa.

---
+++ 4.het/Posix_2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+/*
+Main thread runs for 4 secons.
+Child thread for 8 seconds.
+gcc Posix_2.c -pthread -o Posix_2.exe
+*/
+
+void main_thread();
+
+void *child_thread(void *vargp);
+
+int main() {
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, child_thread, NULL);
+    main_thread();
+    pthread_join(thread_id, NULL);
+    printf("Child thread killed.\n");
+    return (0);
+}
+
+void main_thread() {
+    time_t start_time = time(NULL);
+    while (time(NULL) - start_time < 4) {
+        printf("Main  thread runtime: %d s\n", time(NULL) - start_time);
+        sleep(1);
+    }
+}
+
+void *child_thread(void *vargp) {
+    time_t start_time = time(NULL);
+    while (time(NULL) - start_time < 8) {
+        printf("Child thread runtime: %d s\n", time(NULL) - start_time);
+        sleep(1);
+    }
+}
